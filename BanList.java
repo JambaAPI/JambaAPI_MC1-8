91a24fc69ce8c5d0e8eaaed5256b3c144dbf87b7
AdomaAdoma <adoma_@outlook.com>
2014-10-16 17:18:05 +0400

    Create BanList.java

---
+++ BanList.java
@@ -0,0 +1,23 @@
+package JambaAPI;
+
+import java.util.Date;
+import java.util.Set;
+
+public abstract interface BanList {
+	  public abstract BanEntry getBanEntry(String paramString);
+
+	  public abstract BanEntry addBan(String paramString1, String paramString2, Date paramDate, String paramString3);
+
+	  public abstract Set<BanEntry> getBanEntries();
+
+	  public abstract boolean isBanned(String paramString);
+
+	  public abstract void pardon(String paramString);
+
+	  public static enum Type
+	  {
+	    NAME, 
+
+	    IP;
+	  }
+}
